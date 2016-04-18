8b57e5595c56c5c4819dc9c6574de113832eb006
Joshua Michael <josh@Joshuas-MacBook-Pro.local>
2016-04-18 02:18:47 -0700

    Updated pull_from_abstract with new geo extractor class

---
+++ geography_extractor.py
@@ -0,0 +1,77 @@
+import os, re
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+
+class geography_extractor:
+	def __init__(self, geograpy_path = './GeoLite2-City-Locations.csv'):
+		self.punc_list = ['!','.','/',',','?']
+		self._country_continent_map = {}
+		self._district_country_map =  {}
+		self._city_district_map = {}
+		self._location_set = set()
+		self._city_set = set()
+		self._parse_geograpy(geograpy_path)
+
+
+	def extract_geography(self, text):
+		found_location = set()
+		text = text.replace('\n','')
+		for punc in self.punc_list:
+			text = text.replace(punc, ' ')
+		
+		test_text = ' ' + text + ' '
+
+		for location in self._location_set:
+			test_location = ' ' + location + ' '
+			if test_location in test_text:
+				'''
+				print location
+				try: district = self._city_district_map[location]
+				except KeyError: district = ''
+				try: country = self._district_country_map[location]
+				except KeyError: country = ''
+				try: continent = self._country_continent_map[location]
+				except KeyError: continent = ''
+				if location in self._city_set: city = location
+				else: city = ''
+				full_location = ','.join([continent, country, district, city])
+				'''
+				found_location.add(location)
+				test_text.replace(test_location, ' ')
+		print found_location
+		print "\n"
+		return found_location
+
+	def _parse_geograpy(self, geograpy_path):
+		for line in open(geograpy_path):
+			#line = line.lower()
+			#line = line.replace(',', ' ')
+			#line = re.sub(r'[^\w]', '', line)
+			#print line.split(',')
+			continent = line.split(',')[2]
+			country = line.split(',')[4]
+			district = line.split(',')[6]
+			city = line.split(',')[7]
+			city = city.replace('\n','').split("/")[-1].replace('"','')
+			#print city
+
+			if continent != '' and country != '': self._country_continent_map[country] = continent
+			if country != '' and district != '': self._district_country_map[district] = country
+			if district != '' and city != '': self._city_district_map[city] = district
+
+			if continent != '': self._location_set.add(continent)
+			if country != '': self._location_set.add(country)
+			if district != '': self._location_set.add(district)
+			if city != '': 
+				self._location_set.add(city)
+				self._city_set.add(city)
+
+
+			
+
+if __name__ == '__main__':
+	g = geography_extractor()
+	g.extract_geography('I am from Los Angeles,California.wpe owaifj opa wfoiajf skajf dsah Chino Hills, Fremont')
--- pull_from_abstract.py
+++ pull_from_abstract.py
@@ -1,65 +1,101 @@
 import json, nltk, os, random
+from geography_extractor import geography_extractor
 from pprint import pprint
 
 
-def parse_abstracts(data):
-
-	# Neatly prints out the abstract for comparison with resulting extracted datasets,
-	# which are also printed
-	pprint(data['abstract'])
-	print "\n"
-	
-	json_abstract = data['abstract']
-
-	text = nltk.word_tokenize(json_abstract)
-	sentence = nltk.pos_tag(text)
-
-	# Searches for datasets using basic rule:
-	# ( Proper noun followed by 1 or more of: Noun, Conjunction, Preoposition,
-	# another Proper Noun, Plural Noun, or Determiner ) OR multiple Proper Nouns
-	grammar = r"""
-		NP: {(<NNP><NN|CC|NNP|IN|NNS|DT>+)|<NNP>+}
+class parse_from_abstract:
+	def __init__(self, data, df):
+		self.data = data
+		self.g_extractor = geography_extractor()
+		self.data_file = df
+		self.parse_abstracts(self.data, self.data_file)
 		
-	"""
-	cp = nltk.RegexpParser(grammar)
+	def parse_abstracts(self, data, data_file):
 
+		# Neatly prints out the abstract for comparison with resulting extracted datasets,
+		# which are also printed
+		pprint(self.data['abstract'])
 
-	results = set()
-
-	for item in cp.parse(sentence):
-		names = []
+		#data_file.write(data['abstract'])
+		pprint(self.data['abstract'], self.data_file)
 		
-		if hasattr(item, 'label') and item.label:
+		json_abstract = self.data['abstract']
+		text = nltk.word_tokenize(json_abstract)
+		sentence = nltk.pos_tag(text)
+
+		# Searches for datasets using basic rule:
+		# ( Proper noun followed by 1 or more of: Noun, Conjunction, Preoposition,
+		# another Proper Noun, Plural Noun, or Determiner ) OR multiple Proper Nouns
+		grammar = r"""
+			NP: {(<NNP><NN|CC|NNP|IN|NNS|DT>+)|<NNP>+}
 			
-			# If the item has been selected by our regex grammar rule:
-			if item.label() == 'NP':
-				names.append(' '.join([child[0] for child in item]))
+		"""
+		cp = nltk.RegexpParser(grammar)
+
+
+		results = set()
+
+		for item in cp.parse(sentence):
+			names = []
+			
+			if hasattr(item, 'label') and item.label:
 				
-				# Next five lines are a sloppy way of avoiding names like
-				# J. Doe as well as removing basic single-word selections,
-				# unless they are acronyms. Questionable filter, will be
-				# improved soon.
-				if len(names[0].split(" ")) > 1 and names[0][1] != '.':
-					results.add(names[0])
-				
-				elif sum(1 for l in names[0] if l.isupper()) > 1 and names[0][1] != '.':
-					results.add(names[0]) 
-	print results
-	print '\n'
+				# If the item has been selected by our regex grammar rule:
+				if item.label() == 'NP':
+					names.append(' '.join([child[0] for child in item]))
+					
+					# Next five lines are a sloppy way of avoiding names like
+					# J. Doe as well as removing basic single-word selections,
+					# unless they are acronyms. Questionable filter, will be
+					# improved soon.
+					if len(names[0].split(" ")) > 1 and names[0][1] != '.':
+						results.add(names[0])
+					
+					elif sum(1 for l in names[0] if l.isupper()) > 1 and names[0][1] != '.':
+						results.add(names[0]) 
+
+		# Creates a set of locations that are present in the json file abstract
+		locations = self.g_extractor.extract_geography(json_abstract)
+		print "Locations: ",locations
+		print '\n'
+		print "Results with Locations included: ", results
+		print '\n'
+		pprint(results, self.data_file)
+		pprint(locations, self.data_file)
+		discard_list = []
+		for item1 in locations:
+			for item2 in results:
+				if item1 in item2:
+					discard_list.append(item2)
+		for x in discard_list:
+			results.discard(x)
+		print "Results without locations: ", results
+		# results_less_locations = results.difference(locations)
+		# print results_less_locations
+		# pprint(locations, self.data_file)
+		# pprint(results_less_locations, self.data_file)
+		pprint(results, self.data_file)
+		print '\n'
 
 
-for i in range(4): # Change this value to determine how many abstracts you want tested
-	data = []
+if __name__ == '__main__':
 
-	# The folder jsonFiles, as mentioned in the README, should be in the same directory
-	# as this file, and contain the raw files without additional folders. This can 
-	# change once we test the extraction on all of our abstracts.
+	data_file = open('datasets.txt', 'w+')
 
-	json_file = random.choice(os.listdir('./jsonFiles'))
+	for i in range(2): # Change this value to determine how many abstracts you want tested
+		data = []
 
-	# Prints name of the file that was selected for opening and parsing
-	print "Name of json file: " + json_file
-	with open('./jsonFiles/' + json_file) as f:
-		data = json.load(f)
-	parse_abstracts(data)
+		# The folder jsonFiles, as mentioned in the README, should be in the same directory
+		# as this file, and contain the raw files without additional folders. This can 
+		# change once we test the extraction on all of our abstracts.
+
+		json_file = random.choice(os.listdir('./jsonFiles'))
+
+		# Prints name of the file that was selected for opening and parsing
+		print "Name of json file: " + json_file
+		data_file.write("Name of json file: " + json_file)
+		data_file.write('\n')
+		with open('./jsonFiles/' + json_file) as f:
+			data = json.load(f)
+		pfa = parse_from_abstract(data, data_file)
 
